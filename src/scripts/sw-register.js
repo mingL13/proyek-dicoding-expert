1ae72866694b2d071b271d0c943c724393eed38d
ming <learntocodem88@gmail.com>
2023-06-04 01:26:11 +0700

    third commit

---
+++ src/scripts/sw-register.js
@@ -0,0 +1,17 @@
+import * as WorkboxWindow from "workbox-window";
+
+const swRegister = async () => {
+  if (!("serviceWorker" in navigator)) {
+    console.log("Service Worker not supported in the browser");
+    return;
+  }
+  const wb = new WorkboxWindow.Workbox("./sw.bundle.js");
+  try {
+    await wb.register();
+    console.log("Service Worker Registered");
+  } catch (error) {
+    console.log("Failed to Register Service Worker", error);
+  }
+};
+
+export default swRegister;
--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: {
     app: path.resolve(__dirname, "src/scripts/index.js"),
-    sw: path.resolve(__dirname, "src/scripts/sw.js"),
   },
   output: {
     filename: "[name].bundle.js",
@@ -39,6 +39,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: path.resolve(__dirname, "src/templates/index.html"),
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: "./sw.bundle.js",
     }),
     new CopyWebpackPlugin({
       patterns: [
